790d34ac91405189cb1e714152cbf8a5c4cce1ac
intiinside <inti.poaquiza@epn.edu.ec>
2018-08-13 15:01:49 -0500

    barra busqueda
    terminado

--- examenAngular/src/app/interfaces/ingrediente.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface IngredienteInterface {
-  nombreIngrediente: string;
-  cantidad: number;
-  descripcionPreparacion: string;
-  opcional: boolean;
-  tipoIngrediente: string;
-  necesitaRefrigeracion: boolean;
-  imagenUrl: string;
-}
