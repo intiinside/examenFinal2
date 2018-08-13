898afe11d3f27e2540674c0cf2176d905ee70cfc
intiinside <inti.poaquiza@epn.edu.ec>
2018-08-13 13:21:12 -0500

    inti
    comit

--- examenAngular/src/app/services/carrito.service.ts
+++
@@ -1,8 +0,0 @@
-import {Injectable} from "@angular/core";
-import {PokemonInterface} from "../interfaces/pokemon.interface";
-
-@Injectable()
-export class CarritoService {
-  static pokemonDelCarrito : Array<PokemonInterface>=[];
-  constructor(){}
-}
